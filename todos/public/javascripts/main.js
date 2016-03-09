714eb6471656799c2ff489b7814986c0c9e0fbc4
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2016-03-09 17:02:35 +0100

    Clear popup-container on body-container pjax
    Also, comment pjax code a bit.

--- todos/public/javascripts/main.js
+++ todos/public/javascripts/main.js
@@ -1,21 +1,33 @@
 
 $(function(){
+
+  // Register popup-container pjax, without back-button support
   $(document).pjax('a.popup', '#popup-container', {
     push: false
   });
 
+  // Register body-container pjax, using no cache
   $(document).pjax('a:not(.popup)', '#body-container', {
     maxCacheLength: 0
   });
 
+  // Clear popup container on body-container navigation
+  $(document).on('pjax:complete', function(event) {
+    if ($(event.target).is('#body-container')) {
+      $('#popup-container').empty();
+    }
+  });
+
+  // Send form over ajax, refresh client-side includes,
+  //   and navigate to response url in body-container
   $(document).on('submit', 'form[data-pjax]', function(event) {
     event.preventDefault();
 
     SimpleForm.serialize(event, function(response) {
-      $('#popup-container').empty();
 
       IncludesRefresher.refresh(response.events);
 
+      // Call pjax manually
       $.pjax({
         url: response.url,
         container: '#body-container',
